45af991d1a857d05e09bcdbbe22232bc66a1bf86
AFOP <felipe@ASUSAFOP.localdomain>
2023-04-06 12:20:50 -0500

    update task 0

--- markdown2html.py
+++ markdown2html.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-'''
+"""
 Write a script markdown2html.py that takes an argument 2 strings:
 
 First argument is the name of the Markdown file
@@ -9,7 +9,7 @@ Requirements:
 If the number of arguments is less than 2: print in STDERR Usage: ./markdown2html.py README.md README.html and exit 1
 If the Markdown file doesn’t exist: print in STDER Missing <filename> and exit 1
 Otherwise, print nothing and exit 0
-'''
+"""
 import sys
 import os.path
 import markdown
