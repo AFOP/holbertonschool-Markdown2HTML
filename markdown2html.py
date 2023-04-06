2f72dce01a93d187203a5686e04af85c0d917dcf
AFOP <felipe@ASUSAFOP.localdomain>
2023-04-06 12:28:29 -0500

    update task 0

--- markdown2html.py
+++ markdown2html.py
@@ -12,24 +12,24 @@ If the Markdown file doesn’t exist:
 print in STDER Missing <filename> and exit 1
 Otherwise, print nothing and exit 0
 """
+if __name__ == "__main__":
+    import sys
+    import os.path
+    import markdown
 
-import sys
-import os.path
-import markdown
+    if len(sys.argv) < 3:
+        sys.stderr.write("Usage: {} README.md README.html\n".format(sys.argv[0]))
+        sys.exit(1)
 
-if len(sys.argv) < 3:
-    sys.stderr.write("Usage: {} README.md README.html\n".format(sys.argv[0]))
-    sys.exit(1)
+    md_file = sys.argv[1]
+    if not os.path.isfile(md_file):
+        sys.stderr.write("Missing {}\n".format(md_file))
+        sys.exit(1)
 
-md_file = sys.argv[1]
-if not os.path.isfile(md_file):
-    sys.stderr.write("Missing {}\n".format(md_file))
-    sys.exit(1)
-
-html_file = sys.argv[2]
-with open(md_file, 'r') as f:
-    md = f.read()
-    html = markdown.markdown(md)
-    with open(html_file, 'w') as g:
-        g.write(html)
-sys.exit(0)
+    html_file = sys.argv[2]
+    with open(md_file, 'r') as f:
+        md = f.read()
+        html = markdown.markdown(md)
+        with open(html_file, 'w') as g:
+            g.write(html)
+    sys.exit(0)
